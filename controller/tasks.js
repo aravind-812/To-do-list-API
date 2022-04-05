5bf2a5d8c0a0d52a3a253e77b46fae2a8620b805
aravind-812 <aravindkamarsu32@gmail.com>
2022-04-06 01:22:37 +0530

    Done with all CRUD operations.

--- controller/tasks.js
+++ controller/tasks.js
@@ -38,12 +38,32 @@ const getTask = async (req, res) => {
     }
 }
 
-const updateTask = (req, res) => {
-    res.send("task created")
+const updateTask = async (req, res) => {
+    const { id: taskID } = req.params
+    const updated = await task.findOneAndUpdate({ _id: taskID }, req.body, {
+        new: true,
+        runValidators: true,
+    })
+    if (!updated) {
+        return res.status(404).json({ "msg": `no task with id: ${taskID}` })
+    }
+    res.json({ updated })
+
 }
 
-const deleteTask = (req, res) => {
-    res.send("task created")
+const deleteTask = async (req, res) => {
+    try {
+        const { id: deleteTaskId } = req.params
+        const delTask = await task.findOneAndDelete({ _id: deleteTaskId })
+        if (!oneTask) {
+            return res.status(404).json({ "msg": `no task with id: ${delTask}` })
+        }
+        console.log("done")
+        res.status(200).json({ "Id": `The task has been deleted ${deleteTaskId}` })
+    } catch (error) {
+        console.log(error)
+        res.send({ "error": error._message })
+    }
 }
 
 
