8ca91f2048532d6128109613b6d7e0e087a86fc8
Aravind K <aravind@Aravinds-MacBook-Pro.local>
2022-05-22 22:51:27 +0530

    formating

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000
-const tasks = require('./routes/tasks')
-const connectDB = require("./db/connect")
-require('dotenv').config()
+const port = 3000;
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 //Middleware setup
 app.use(express.json());
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
 //Routes
-app.use('/api/v1/tasks', tasks)
-
+app.use("/api/v1/tasks", tasks);
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, () => console.log('We are listening at 3000...'))
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => console.log("We are listening at 3000...."));
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 start();
-
-
-
-
-
